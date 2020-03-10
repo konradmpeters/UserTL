ffd488660b79cca093f00e7cb51381156b25c6c0
Peters <KMPeters@rivcoda.org>
2020-03-10 11:06:32 -0700

    Fixing ports

--- app.js
+++ app.js
@@ -16,23 +16,23 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //Prevent CORS errors
-// app.use((req,res,next) => {
-//     res.header("Access-Control-Allow-Origin","*"); // * opens up to all pages but could restrict by added page url
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     if (req.method === "OPTION") {
-//         res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
-//         return res.status(200).json({});
-//     };
-//     next();  // So that the other routes can take over.
-// });
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin","*"); // * opens up to all pages but could restrict by added page url
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTION") {
+        res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    };
+    next();  // So that the other routes can take over.
+});
 
 app.use('/users', userRoutes);
+
+// Disabled Application Routes
 app.use("/apps", appRoutes);
-
-
 app.use('/', express.static('dist/UserTL'));
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 
 
 const app = require('./app');
-const Port = process.env.port || 443;  //3443
+const Port = process.env.port || 3443;  //443
+//const http = require('http');
 const https = require('https');
+//const express = require('express');
 const fs = require('fs');
+
+// var domain = 'localhost';
+
+// app.get('*', function(req, res){
+//     // redirect to HTTPS
+//     res.redirect('https://' + domain + req.path);
+// });
+
+// http.createServer(app).listen(80, function(){
+//     console.log('HTTP listening on port 80');
+// });
 
 // app.get('/', (req, res) => {
 //     console.log('Hello HTTPS!');
 //     res.send('Hello HTTPS!');
 // })
+
+//var appSecure = express();
+// configure your app here
+
+// var options = {
+// //   key: fs.readFileSync('ssl_key.pem'),
+// //   cert: fs.readFileSync('ssl_cert.crt')
+//     key: fs.readFileSync('server.key'),
+//     cert: fs.readFileSync('server.cert')
+// };
+
+// https.createServer(options, app).listen(443, function(){
+//     console.log('HTTPS listening on port 443');
+// });
 
 https.createServer({
     key: fs.readFileSync('server.key'),
